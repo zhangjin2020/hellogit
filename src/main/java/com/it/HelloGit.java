4fd213040b46a08b8dc205c24c8e2086fe8e8d5f
2318435581@qq.com <2318435581@qq.com>
2020-05-24 16:30:46 +0800

    更新1

---
+++ src/main/java/com/it/HelloGit.java
@@ -0,0 +1,9 @@
+package com.it;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hhwww");
+        System.out.println("更新1");
+    }
+}
